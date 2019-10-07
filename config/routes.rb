7c062ab90e3a45d252083083a1310f8f9ffdc7fa
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2019-10-07 12:09:26 -0300

    rutas nested de todos los recursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :crops
-  resources :product_applications
-  resources :problems
-  resources :plantations
   resources :products
   resources :varieties
   resources :type_problems
   resources :species
   resources :farms do
-    resources :plots
+    resources :plots do
+      resources :crops
+      resources :problems
+      resources :plantations
+      resources :product_applications
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
