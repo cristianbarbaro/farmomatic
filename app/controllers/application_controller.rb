6a114b02985135b90ff23e50960c6367d32f5fd1
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2019-12-10 10:26:13 -0300

    verificaciones de permisos con mensajes de alertas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,15 @@ class ApplicationController < ActionController::Base
 
   private
     def check_permissions_owner
-      head 403 unless current_user.has_role? :owner, @farm
+      #head 403 unless current_user.has_role? :owner, @farm
+      return unless !current_user.has_role? :owner, @farm
+      redirect_to root_path, notice: "Debe tener rol de owner para acceder al recurso."
     end
 
     def check_permissions_expert
-      head 403 unless current_user.has_role? :expert
+      return unless !current_user.has_role? :expert
+      #head 403 unless current_user.has_role? :expert
+      redirect_to root_path, notice: "Solo se permite acceso a usuarios expertos."
     end
 
 end
