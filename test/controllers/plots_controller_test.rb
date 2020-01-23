184f19b158b0a3dc629833f96dc9809b8b7f89f2
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2020-01-23 20:28:35 -0300

    tests de controlador de lotes

--- test/controllers/plots_controller_test.rb
+++ test/controllers/plots_controller_test.rb
@@ -2,47 +2,56 @@ require 'test_helper'
 
 class PlotsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @plot = plots(:one)
+    sign_in users(:owner_user)
+    @plot_one = plots(:plot_one)
+    @plot_three = plots(:plot_three)
   end
 
   test "should get index" do
-    get plots_url
+    get farm_plots_url(farm_id: 1)
     assert_response :success
   end
 
   test "should get new" do
-    get new_plot_url
+    get new_farm_plot_url(farm_id: 1)
     assert_response :success
   end
 
   test "should create plot" do
     assert_difference('Plot.count') do
-      post plots_url, params: { plot: { description: @plot.description, greenhouse: @plot.greenhouse, identifier: @plot.identifier, surface: @plot.surface } }
+      post farm_plots_url(farm_id: 1), params: {plot: { description: @plot_one.description, greenhouse: @plot_one.greenhouse, identifier: @plot_one.identifier, surface: @plot_one.surface } }
     end
-
-    assert_redirected_to plot_url(Plot.last)
+    assert_redirected_to farm_plots_url(farm_id: 1)
   end
 
   test "should show plot" do
-    get plot_url(@plot)
+    get farm_plot_url( 1, @plot_one)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_plot_url(@plot)
+    get edit_farm_plot_url(1, @plot_one)
     assert_response :success
   end
 
   test "should update plot" do
-    patch plot_url(@plot), params: { plot: { description: @plot.description, greenhouse: @plot.greenhouse, identifier: @plot.identifier, surface: @plot.surface } }
-    assert_redirected_to plot_url(@plot)
+    patch farm_plot_url(1, @plot_one), params: { plot: { description: @plot_one.description, greenhouse: @plot_one.greenhouse, identifier: @plot_one.identifier, surface: @plot_one.surface } }
+    assert_redirected_to farm_plot_url(1, @plot_one)
   end
 
-  test "should destroy plot" do
-    assert_difference('Plot.count', -1) do
-      delete plot_url(@plot)
+  
+  #test "should destroy plot without association" do
+  #  assert_difference('Plot.count', -1) do
+  #    delete farm_plot_url(2, @plot_three)
+  #  end
+  #
+  #  assert_redirected_to farm_plots_url(farm_id: 2)
+  #end
+
+  test "should not destroy plot with plantation" do
+    assert_raises(ActiveRecord::InvalidForeignKey) do
+      delete farm_plot_url(1, @plot_one)
     end
-
-    assert_redirected_to plots_url
   end
+
 end
