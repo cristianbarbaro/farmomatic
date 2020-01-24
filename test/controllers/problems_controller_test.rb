c665eb6cba71a57abe5ce86203f0f242b7e3a59e
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2020-01-24 11:07:19 -0300

    Agregado test de controlador de problemas y editado de plantaciones

--- test/controllers/problems_controller_test.rb
+++ test/controllers/problems_controller_test.rb
@@ -2,47 +2,76 @@ require 'test_helper'
 
 class ProblemsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @problem = problems(:one)
+    @problem_one = problems(:problem_one)
   end
 
   test "should get index" do
-    get problems_url
+    get farm_plot_problems_url(farm_id: 1, plot_id: 1)
     assert_response :success
   end
 
   test "should get new" do
-    get new_problem_url
+    get new_farm_plot_problem_url(farm_id:1, plot_id:1)
     assert_response :success
   end
 
   test "should create problem" do
     assert_difference('Problem.count') do
-      post problems_url, params: { problem: { comment: @problem.comment, plot_id: @problem.plot_id, type_problem_id: @problem.type_problem_id } }
+      post farm_plot_problems_url(farm_id:1, plot_id:1), params: { problem: { comment: @problem_one.comment, plot_id: @problem_one.plot_id, type_problem_id: @problem_one.type_problem_id } }
     end
 
-    assert_redirected_to problem_url(Problem.last)
+    assert_redirected_to farm_plot_problems_url(farm_id: 1, plot_id: 1)
   end
 
   test "should show problem" do
-    get problem_url(@problem)
+    get farm_plot_problem_url(1, 1, @problem_one)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_problem_url(@problem)
+    get edit_farm_plot_problem_url(1, 1, @problem_one)
     assert_response :success
   end
 
   test "should update problem" do
-    patch problem_url(@problem), params: { problem: { comment: @problem.comment, plot_id: @problem.plot_id, type_problem_id: @problem.type_problem_id } }
-    assert_redirected_to problem_url(@problem)
+    patch farm_plot_problem_url(1, 1, @problem_one), params: { problem: { comment: @problem_one.comment, plot_id: @problem_one.plot_id, type_problem_id: @problem_one.type_problem_id } }
+    assert_redirected_to farm_plot_problem_url(1, 1, @problem_one)
   end
 
   test "should destroy problem" do
     assert_difference('Problem.count', -1) do
-      delete problem_url(@problem)
+      delete farm_plot_problem_url(1, 1, @problem_one)
     end
 
-    assert_redirected_to problems_url
+    assert_redirected_to farm_plot_problems_url(farm_id:1, plot_id:1)
   end
+
+  test "should not create plantation if user is not owner of farm" do
+    sign_in users(:viewer_user)
+    assert_difference('Problem.count', 0) do
+      post farm_plot_problems_url(farm_id:1, plot_id:1), params: { problem: { comment: @problem_one.comment, plot_id: @problem_one.plot_id, type_problem_id: @problem_one.type_problem_id } }
+    end
+    assert_redirected_to farm_plot_problems_url(farm_id: 1, plot_id: 1)
+  end
+
+  test "should not get edit if user is viewer" do
+    sign_in users(:viewer_user)
+    get edit_farm_plot_problem_url(1, 1, @problem_one)
+    assert_redirected_to farm_plot_problems_url(farm_id: 1, plot_id: 1)
+  end
+
+  test "should not update problem if user is viewer" do
+    sign_in users(:viewer_user)
+    patch farm_plot_problem_url(1, 1, @problem_one), params: { problem: { comment: @problem_one.comment, plot_id: @problem_one.plot_id, type_problem_id: @problem_one.type_problem_id } }
+    assert_redirected_to farm_plot_problems_url(farm_id: 1, plot_id: 1)
+  end
+
+  test "should not destroy if user is viewer" do
+    sign_in users(:viewer_user)
+    assert_difference('Problem.count', 0) do
+      delete farm_plot_problem_url(1, 1, @problem_one)
+    end
+    assert_redirected_to farm_plot_problems_url(farm_id: 1, plot_id: 1)
+  end
+
 end
