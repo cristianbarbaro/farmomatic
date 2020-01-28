17886419a5c3c976e85bcd42e76c4f724cc43c01
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2020-01-28 17:50:07 -0300

    se permite name de usuario cuando se registra o edita un perfil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
     def check_permissions_owner
@@ -17,6 +18,11 @@ class ApplicationController < ActionController::Base
     def authorized_admin
       return unless !current_user.has_role? :admin
       redirect_to root_path, notice: '¡Solo se permite el acceso a usuarios administradores!'
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password])
     end
 
 end
